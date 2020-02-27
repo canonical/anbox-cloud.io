5a56e6e1612ce342055c4e72cc60987d0670e400
Carlos Wu <carlos.fei@canonical.com>
2020-02-27 10:49:57 +0000

    Script to detect device OS

---
+++ static/js/detect-device.js
@@ -0,0 +1,11 @@
+export default function mobileDevice(navigator) {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i))
+        return true
+    return false
+}
