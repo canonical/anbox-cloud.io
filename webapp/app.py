f70e1251e685d3366812238a9061cd7b800fc980
Beth Collins <bethcollinsliv92@gmail.com>
2021-05-06 16:15:35 +0100

    Add media query for centre align

--- webapp/app.py
+++ webapp/app.py
@@ -111,14 +111,14 @@ def index():
     return flask.render_template("index.html")
 
 
-@app.route("/thank-you")
-def thank_you():
-    return flask.render_template("thank-you.html")
-
-
 @app.route("/contact-us")
 def contact_us():
     return flask.render_template("contact-us.html")
+
+
+@app.route("/thank-you")
+def thank_you():
+    return flask.render_template("thank-you.html")
 
 
 @app.route("/terms")
